8d66660537d9a354a1aba483a8cf466cdb329b23
Abhishek <Abhishek@DESKTOP-OFDMCBR>
2025-11-02 10:43:39 +0530

    Add project files.

---
+++ BreakItMakeIt_Exercises/Chapter_01_TDDFundamentals/TDD_Beginner.cs
@@ -0,0 +1,24 @@
+﻿using BreakItMakeIt_Exercises.Chapter_01_TDDFundamentals.Beginner;
+
+namespace BreakItMakeIt_Exercises;
+
+public class TDD_Beginner
+{
+    private Calculator _calculator;
+    [SetUp]
+    public void Setup()
+    {
+        _calculator = new Calculator();
+    }
+
+    [TestCase(1, 2, 3)]
+    [TestCase(3, 4, 7)]
+    public void AddSumOfTwoNumbers(int a, int b, int expectedSum)
+    {
+        //Arrange - setup
+        //Act
+        int actualSum = _calculator.Sum(a, b);
+        //Assert
+        Assert.That(actualSum, Is.EqualTo(expectedSum));
+    }
+}
---
+++ BreakItMakeIt_Exercises/Chapter_01_TDDFundamentals/TDD_Intermediate.cs
@@ -0,0 +1,15 @@
+﻿namespace BreakItMakeIt_Exercises;
+
+public class TDD_Intermediate
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
