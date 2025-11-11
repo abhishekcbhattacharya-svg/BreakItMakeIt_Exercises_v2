f116a85117031d849c022aad65b3f65bf0d56570
Abhishek <Abhishek@DESKTOP-OFDMCBR>
2025-11-11 21:33:36 +0530

    Add second chapter assignment

---
+++ BreakItMakeIt_Exercises/Chapter_02_Framework_vs_Library/Library/UserModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProject.Models;
+
+namespace BreakItMakeIt_Exercises.Chapter_02_Framework_vs_Library.Library
+{
+    public class UserModelValidator : AbstractValidator<UserModel>
+    {
+        public UserModelValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Age).InclusiveBetween(18, 60).WithMessage("Age must be between 18 and 60");
+        }
+    }
+}
---
+++ WebProject/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebProject.Models;
+
+namespace WebProject.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create(UserModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok("User created");
+        }
+    }
+}
